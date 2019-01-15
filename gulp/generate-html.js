4c94333fad6288693eab72d1daaeb761d0caadec
Regulyarniy Nikolay <regularniy@gmail.com>
2019-01-15 15:58:47 +1000

    adding posthtml-expressions

--- gulp/generate-html.js
+++ gulp/generate-html.js
@@ -1,11 +1,13 @@
 const {src, dest} = require(`gulp`);
 const posthtml = require(`gulp-posthtml`);
 const include = require(`posthtml-include`);
+const expressions = require(`posthtml-expressions`);
 
 const generateHtml = () => {
   return src(`source/*.html`)
   .pipe(posthtml([
-    include()
+    include(),
+    expressions()
   ]))
   .pipe(dest(`dist`));
 };
